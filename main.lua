0d6df2f8d58f93f553642addd48a1d52bd5806aa
ignisdesign <brent@ignisdesign.com>
2017-01-12 11:12:43 -1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,78 +12,67 @@ https://docs.coronalabs.com/api/library/composer/index.html
 
 -- ]]
 
-local composer = require "composer"
+local composer = require( "composer" )
 
 -- Removes status bar on iOS
 display.setStatusBar( display.HiddenStatusBar ) 
 
 -- Removes bottom bar on Android 
-if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
-  native.setProperty( "androidSystemUiVisibility", "lowProfile" )
+if ( system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 ) then
+	native.setProperty( "androidSystemUiVisibility", "lowProfile" )
 else
-  native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
+	native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
 end
 
--- reserve audio for menu, bgsound, and wheels
-audio.reserveChannels(3)
+-- Reserve audio for menu, background sounds, and wheels
+audio.reserveChannels( 3 )
 
--- are we running on a simulator?
+-- Are we running in a simulator?
 local isSimulator = "simulator" == system.getInfo( "environment" )
-local isMobile = ("ios" == system.getInfo("platform")) or ("android" == system.getInfo("platform"))
+local isMobile = ( "ios" == system.getInfo("platform") ) or ( "android" == system.getInfo("platform") )
 
--- if we are load our visual monitor that let's a press of the "F"
--- key show our frame rate and memory usage, "P" to show physics
+-- Load visual monitor and allow press of the "F" key to show frame rate and memory usage
 if isSimulator then 
 
-  -- show FPS
-  local visualMonitor = require( "com.ponywolf.visualMonitor" )
-  local visMon = visualMonitor:new()
-  visMon.isVisible = false
+	-- Load visual monitor
+	local visualMonitor = require( "com.ponywolf.visualMonitor" )
+	local visMon = visualMonitor:new()
+	visMon.isVisible = false  -- Initially hide it
 
-  -- show/hide physics
-  local function debugKeys( event )
-    local phase = event.phase
-    local key = event.keyName
-    if phase == "up" then
-      if key == "p" then
-        physics.show = not physics.show
-        if physics.show then 
-          physics.setDrawMode( "hybrid" ) 
-        else
-          physics.setDrawMode( "normal" )  
-        end
-      elseif key == "f" then
-        visMon.isVisible = not visMon.isVisible 
-      end
-    end
-  end
-  Runtime:addEventListener( "key", debugKeys )
+	-- Show/hide physics
+	local function debugKeys( event )
+		local phase = event.phase
+		local key = event.keyName
+		if phase == "up" then
+			if key == "f" then
+				visMon.isVisible = not visMon.isVisible 
+			end
+		end
+	end
+	Runtime:addEventListener( "key", debugKeys )
 end
 
+-- This module turns gamepad axis events and mobile accelometer events into keyboard events
+-- so we don't have to write separate code for joystick and keyboard control
+require( "com.ponywolf.joykey" ).start()
 
--- this module turns gamepad axis events and mobile accelometer events
--- into keyboard events so we don't have to write separate code 
--- for joystick and keyboard control
-require("com.ponywolf.joykey").start()
-
--- add virtual buttons to mobile 
-system.activate("multitouch")
+-- Add virtual buttons to mobile 
+system.activate( "multitouch" )
 if isMobile or isSimulator then
-  local vjoy = require "com.ponywolf.vjoy"
-  local right = vjoy.newButton("scene/game/img/ui/wheelButton.png", "right")
-  local left = vjoy.newButton("scene/game/img/ui/footButton.png", "left")  
-  local jump = vjoy.newButton("scene/game/img/ui/jumpButton.png", "space")  
-  right.x, right.y = display.screenOriginX + 256 + 32, display.screenOriginY + display.contentHeight - 96
-  left.x, left.y =  display.screenOriginX + 128,display.screenOriginY + display.contentHeight - 96 - 32  
-  jump.x, jump.y = -display.screenOriginX + display.contentWidth - 128, display.screenOriginY + display.contentHeight - 96
-  right.xScale, right.yScale = 0.5, 0.5
-  left.xScale, left.yScale = 0.5, 0.5
-  jump.xScale, jump.yScale = 0.5, 0.5
+	local vjoy = require( "com.ponywolf.vjoy" )
+	local right = vjoy.newButton( "scene/game/img/ui/wheelButton.png", "right" )
+	local left = vjoy.newButton( "scene/game/img/ui/footButton.png", "left" )  
+	local jump = vjoy.newButton( "scene/game/img/ui/jumpButton.png", "space" )  
+	right.x, right.y = display.screenOriginX + 256 + 32, display.screenOriginY + display.contentHeight - 96
+	left.x, left.y =  display.screenOriginX + 128,display.screenOriginY + display.contentHeight - 96 - 32  
+	jump.x, jump.y = -display.screenOriginX + display.contentWidth - 128, display.screenOriginY + display.contentHeight - 96
+	right.xScale, right.yScale = 0.5, 0.5
+	left.xScale, left.yScale = 0.5, 0.5
+	jump.xScale, jump.yScale = 0.5, 0.5
 end
 
--- go to menu screen
-composer.gotoScene( "scene.menu", { params={ } } )
+-- Go to menu screen
+composer.gotoScene( "scene.menu", { params={} } )
 
--- uncomment to delete hiscores
+-- Uncomment to delete high scores
 --system.deletePreferences( "app", { "scores" } )
-
